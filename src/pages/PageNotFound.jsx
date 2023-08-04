9a9f0b92bb0f418fa49dd5b9096bd059b754f7a9
Ronnie Kaito Imagawa <ronnie.imagawa@gmail.com>
2023-08-04 17:54:18 +0200

    started routing paths

---
+++ src/pages/PageNotFound.jsx
@@ -0,0 +1,13 @@
+import PageNav from "../components/PageNav";
+
+function PageNotFound() {
+  <PageNav />;
+  return (
+    <div>
+      <PageNav />
+      <h1>Not found</h1>
+    </div>
+  );
+}
+
+export default PageNotFound;

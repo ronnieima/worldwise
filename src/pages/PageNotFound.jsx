c4b0baabd164f2557983ed945ec28e4d810c46cb
Ronnie Kaito Imagawa <ronnie.imagawa@gmail.com>
2023-08-06 09:23:04 +0200

    import styling and add navbar routing

--- src/pages/PageNotFound.jsx
+++ src/pages/PageNotFound.jsx
@@ -1,13 +1,7 @@
-import PageNav from "../components/PageNav";
-
-function PageNotFound() {
-  <PageNav />;
+export default function PageNotFound() {
   return (
     <div>
-      <PageNav />
-      <h1>Not found</h1>
+      <h1>Page not found 😢</h1>
     </div>
   );
 }
-
-export default PageNotFound;

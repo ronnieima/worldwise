b696c5ee7ec598fdf7123fc013ebb6d920f34866
Ronnie Kaito Imagawa <ronnie.imagawa@gmail.com>
2023-08-06 09:36:21 +0200

    setup app page

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,17 @@
+import AppNav from "./AppNav";
+import Footer from "./Footer";
+import Logo from "./Logo";
+import styles from "./Sidebar.module.css";
+
+function Sidebar() {
+  return (
+    <aside className={styles.sidebar}>
+      <Logo />
+      <AppNav />
+      <p>List of Cities</p>
+      <Footer />
+    </aside>
+  );
+}
+
+export default Sidebar;

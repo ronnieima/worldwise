f6b39879143fbbb28b1bcea8fcd3541c0dcac777
Ronnie Kaito Imagawa <ronnie.imagawa@gmail.com>
2023-08-07 16:29:45 +0200

    add nested routing

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,6 +1,6 @@
 import styles from "./Map.module.css";
 function Map() {
-  return <div className={styles.mapContainer}></div>;
+  return <div className={styles.mapContainer}>Map</div>;
 }
 
 export default Map;
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,3 +1,4 @@
+import { Outlet } from "react-router-dom";
 import AppNav from "./AppNav";
 import Footer from "./Footer";
 import Logo from "./Logo";
@@ -8,7 +9,7 @@ function Sidebar() {
     <aside className={styles.sidebar}>
       <Logo />
       <AppNav />
-      <p>List of Cities</p>
+      <Outlet />
       <Footer />
     </aside>
   );
